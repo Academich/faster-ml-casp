0c58bd6154571305e9dfe37bc1c3fa9ef1b2a349
AndroNata <sazhenovanatalia@gmail.com>
2025-07-30 21:21:52 +0100

    Deleted unnecessary files

--- external_modelzoo/ssbenchmark/ssmodels/__init__.py
+++ external_modelzoo/ssbenchmark/ssmodels/__init__.py
@@ -1,7 +1,4 @@
 from .base_model import Registries
 
-from .model_chemformer import model_chemformer
-from .model_localretro import model_localretro
 from .model_transformer import model_transformer
-# from .model_mhn_react import model_mhn_react
 
