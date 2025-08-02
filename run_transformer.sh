30dc876233b5866eb1d9bc52c310b9437fc60e80
AndroNata <nm.sazhenova@physics.msu.ru>
2025-08-02 21:51:24 +0200

    Provided configs for transformer and medusa

--- run_transformer.sh
+++ run_transformer.sh
@@ -2,11 +2,11 @@
 
 SMILES=caspyrus_10k.smi
 OUTPUT=caspyrus_10k
-CONFIG=configs/transformer_medusa_default_config.yml
+CONFIG=configs/transformer_transformer_default_config.yml
 PROCESSES=0
 
 CUDA_VISIBLE_DEVICES=4 aizynthcli --config $CONFIG \
 --smiles $SMILES \
---output ${OUTPUT}_medusa.json \
+--output ${OUTPUT}_transformer.json \
 --nproc $PROCESSES \
  --log_to_file
